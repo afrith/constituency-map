7ad174c7cba7da30e66fc6ae6e47a60349fbb4bb
Adrian Frith <adrianf@da.org.za>
2024-07-07 02:17:10 +0200

    title

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,8 +5,13 @@ export default function Layout ({ children }: PropsWithChildren): JSX.Element {
   return (
     <Container fluid>
       <div className='d-flex flex-column' style={{ height: '100%' }}>
-        <div>
-          <h2>Hypothetical demarcation of South Africa into single-member electoral districts</h2>
+        <div className='d-flex flex-row'>
+          <h2 style={{ flexGrow: 1 }}>
+            Hypothetical demarcation of South Africa into single-member constituencies
+          </h2>
+          <div style={{ textWrap: 'nowrap' }}>
+            by Adrian Frith
+          </div>
         </div>
         <div style={{ flexGrow: 1 }}>
           {children}
