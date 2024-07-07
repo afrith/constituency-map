bc457e3b0230372fe39bd8b23dd4a325ebca4545
Adrian Frith <adrianf@da.org.za>
2024-07-07 14:12:26 +0200

    add 'more info' link with responsive layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -5,12 +5,29 @@ export default function Layout ({ children }: PropsWithChildren): JSX.Element {
   return (
     <Container fluid>
       <div className='d-flex flex-column h-100'>
-        <div className='d-flex flex-column flex-md-row align-items-start align-items-md-end'>
-          <h2 style={{ flexGrow: 1 }}>
+        <div className='d-none d-md-flex flex-row align-items-baseline justify-content-between'>
+          <h2>
             Hypothetical demarcation of South Africa into single-member constituencies
           </h2>
-          <div className='ms-md-3 mb-2 author'>
+          <div className='ms-4 more-info'>
+            <a target='_blank' href='https://adrian.frith.dev/single-member-constituencies/' rel='noreferrer'>more info</a>
+          </div>
+          <div className='ms-4 author'>
             by <a target='_blank' href='https://adrian.frith.dev/' rel='noreferrer'>Adrian Frith</a>
+          </div>
+        </div>
+
+        <div className='d-block d-md-none flex-column align-items-start'>
+          <h2>
+            Hypothetical demarcation of South Africa into single-member constituencies
+          </h2>
+          <div className='mb-2 d-flex flex-row align-items-baseline justify-content-between'>
+            <div className='more-info'>
+              <a target='_blank' href='https://adrian.frith.dev/single-member-constituencies/' rel='noreferrer'>more info</a>
+            </div>
+            <div className='author'>
+              by <a target='_blank' href='https://adrian.frith.dev/' rel='noreferrer'>Adrian Frith</a>
+            </div>
           </div>
         </div>
         <div style={{ flexGrow: 1 }}>
